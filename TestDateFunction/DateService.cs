9ad082db516054c8848668497d8e94a00db70608
SChernikoff <chernikoff80@gmail.com>
2022-04-13 01:59:49 +0300

    Add project files.

---
+++ TestDateFunction/DateService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestDateFunction
+{
+    public class DateService
+    {
+        public void PrintDateDiff(DateTime startDate, DateTime endDate)
+        {
+            //упорядочим даты
+            var dates = DateToSortOrder(startDate, endDate);
+            startDate = dates.Item1;
+            endDate = dates.Item2;
+           
+            var totalDays = (endDate - startDate).TotalDays;
+            var years = GetYears(startDate, endDate);
+            var months = GetMonths(startDate, endDate, years);
+            var days = GetDays(startDate, endDate, years, months);
+
+            Console.WriteLine(years);
+            Console.WriteLine(months);
+            Console.WriteLine(days);
+        }
+
+        private int GetYears(DateTime startDate, DateTime endDate)
+        {
+            return endDate.Year - startDate.Year;
+        }
+
+        //получить разницу месяцев
+        private int GetMonths(DateTime startDate, DateTime endDate, int yearsTotal)
+        {
+            var result = 0;
+            endDate = endDate.AddYears(-yearsTotal);
+            var dates = DateToSortOrder(startDate, endDate);
+            result = dates.Item2.Month - dates.Item1.Month;
+            return result;
+        }
+
+        //получить разницу дней
+        private int GetDays(DateTime startDate, DateTime endDate, int yearsTotal,int monthsTotal)
+        {
+            var result = 0;
+            endDate = endDate.AddYears(-yearsTotal);
+            endDate = endDate.AddMonths(-monthsTotal);
+            var dates = DateToSortOrder(startDate, endDate);
+            result = dates.Item2.Day - dates.Item1.Day;
+            return result;
+        }
+
+
+        //упорядочить даты
+        private (DateTime, DateTime) DateToSortOrder(DateTime startDate, DateTime endDate)
+        {
+            var list = new List<DateTime>();
+            list.Add(startDate);
+            list.Add(endDate);
+            (DateTime, DateTime) result = (list.Min(), list.Max());
+            return result;
+        }
+    }
+}
---
+++ TestDateFunction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TestDateFunction
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            DateService ds = new DateService();
+            ds.PrintDateDiff(new DateTime(2020, 06, 30), new DateTime(2021, 07, 31));
+           
+        }
+
+        
+    }
+}
+//Задача:
+
+//Требуется написать функцию, которая принимает на вход две даты, а на выходе печатает в консоль строку с информацией о количестве полных лет, месяцев и дней между этими датами.
+
+//Пример:
+
+//DateDiff(new DateTime(2020, 06, 30), new DateTime(2021, 07, 31));
+
+//// Полных лет 1, месяцев 1, дней 1
